25130867bf09fb26a8c45b5eb1b0ae50fa9b9ce9
HRITIK <katariahritik72@gmail.com>
2021-04-22 09:24:20 +0530

    objects in javascript

---
+++ Module1/Lec1/intro.js
@@ -0,0 +1,89 @@
+//System.out.println("");
+// cout<<' '<<endl;
+console.log("Hello World !!!");
+
+// Data  Types ==>
+
+//Javascript => Number(int,float,double), boolean , String( " ,"), undefined, null,object
+
+// Datatype name = value;
+//int a = 10;
+
+// ES6 ==> Ecma Script 6
+// Two Keyword let and const
+
+// Dynamic casting
+let a = 10; // you have declared a variable with name and initialized it with value 10;
+
+// let keyword ==> block scoped
+console.log(a);
+if(true){
+    let a = 20;
+    console.log(a);
+}
+console.log(a);
+
+// const ==> constant ==> block scoped and constant
+
+// declare and assign
+const pi = 3.14;
+console.log(pi);
+
+let c = true;
+let d = 3.14;
+let e = 'Hey i am  string';
+let f = undefined;
+// console.log(g);  // g is undefined
+
+// Non Primitive
+
+//Arrays==>
+
+let values =  [1,2,3,4,5,6,,7,8,9,10];
+console.log(values);
+
+//add some data in values array
+values.push("Captain america !!");
+console.log(values);
+
+// Function, object,string,int,boolean
+
+console.log(values.pop());
+
+console.log(values);
+//push => add at the end 
+//pop => delete from the end
+
+//shift => delete a  element from the starting
+//unshift => add a element in the starting of the array
+console.log(values.shift());
+console.log(values);
+
+// Objects => Key values ka pair
+//Keys => unique
+
+let obj = {
+    name:"Steve Rogers",
+    place:"Queens",
+    movies:["captain america","winter soldier",{
+    partner:"falcon",
+    weakness : ["brainwash"]
+
+    }]
+}
+
+console.log(obj.movies[2].weakness[0].substring(1,5))
+// get a value of a key in object
+// console.log(obj.name);
+// console.log(obj.movies);
+
+let key ="place";
+// console.log(obj.key); //=> it will  check if there is a key named "key"
+
+///square brackets notation
+obj["place"];
+obj["Full Name"];
+
+obj.skills = ["martial arts" , "taekwonndo"];
+obj.place = "New york";
+console.log(obj);
